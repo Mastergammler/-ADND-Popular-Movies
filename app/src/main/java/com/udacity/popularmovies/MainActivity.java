c8ca4afe878fee85797abcc3f275da4072942ee7
mastergammler <raphael-hoppe@gmx.de>
2020-03-30 10:03:24 +0200

    Added async task, api access working

--- app/src/main/java/com/udacity/popularmovies/MainActivity.java
+++ app/src/main/java/com/udacity/popularmovies/MainActivity.java
@@ -2,6 +2,8 @@ package com.udacity.popularmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -9,24 +11,42 @@ import com.udacity.popularmovies.networking.NetworkingUtil;
 import com.udacity.popularmovies.themoviedb.api.MovieDbUrlBuilder;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity{
 
+    @SuppressLint("StaticFieldLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = findViewById(R.id.tv_default);
+        final TextView tv = findViewById(R.id.tv_default);
 
-        try
-        {
-            String jsonResult = NetworkingUtil.getResponseFromHttpRequest(MovieDbUrlBuilder.getMoviesByPopularityURL());
-            tv.setText(jsonResult);
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
+        AsyncTask task = new AsyncTask<URL, Void, String>(){
+            @Override
+            protected void onPreExecute() {
+                tv.setText("Loading Data ...");
+            }
+
+            @Override
+            protected String doInBackground(URL... urls) {
+                String jsonResult = null;
+                try
+                {
+                    jsonResult = NetworkingUtil.getResponseFromHttpRequest(urls[0]);
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+                return jsonResult;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                tv.setText(s);
+            }
+        }.execute(MovieDbUrlBuilder.getMoviesByPopularityURL());
     }
 }
